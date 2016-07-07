949371b464cd837404be3ef0bb7edccb63538a2b
aFraley <humanimalmammal@webtv.net>
2016-07-07 10:14:12 -0500

    added a few more analyzations

--- analyze.py
+++ analyze.py
@@ -12,6 +12,52 @@ results = [die1.roll() + die2.roll() for i in tqdm(range(1000000))]
 # Store the frequencies of results in a list.
 frequencies = [results.count(i) for i in range(2, die1.num_sides * 2 + 1)]
 
+######################
+# Central Tendencies #
+######################
+def mean(x):
+    """Determine the mean result."""
+    return str(sum(x) / len(x))
+
+
+
+def median(v):
+    """Determine the median result."""
+    n = len(v)
+    sorted_v = sorted(v)
+    midpoint = n // 2
+
+    if n % 2 == 1:
+        # if odd, return the middle value
+        return str(sorted_v[midpoint])
+    else:
+        # if even, return the average of the middle values
+        lo = midpoint - 1
+        hi = midpoint
+        return str((sorted_v[lo] + sorted_v[hi]) / 2)
+
+
+def quantile(x, p):
+    """Determine a value based on its percentage."""
+    p_index = int(p * len(x))
+    return str(sorted(x)[p_index])
+
+
+##############
+# Dispersion #
+##############
+def data_range(x):
+    return str(max(x) - min(x))
+
+# Execute tests and save the results.
+with open('data.txt', 'w') as f:
+    f.write('Two D6 Analysis')
+    f.write('\nMean result: ' + mean(results))
+    f.write('\nMedian result: ' + median(results))
+    f.write('\nQuantile result: ' + quantile(results, .90))
+    f.write('\nData Range: ' + data_range(results))
+
+
 # Visualize the results.
 hist = Bar()
 
